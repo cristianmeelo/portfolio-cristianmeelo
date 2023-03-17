cf0bf40559b661258e39585383f29b3f22af4d4b
Cristian Melo <cristianmeelo@gmail.com>
2023-03-17 18:55:26 -0300

    :lipstick: style: organized code

--- portfolio-cristianmelo/src/App.tsx
+++ portfolio-cristianmelo/src/App.tsx
@@ -1,71 +1,43 @@
 import { useState } from "react"
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import type { MenuProps } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+import { Layout, Menu, theme } from "antd"
+import type { MenuProps } from "antd"
 import "./styles/App.styles.css"
+import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons"
+const { Header, Content, Footer, Sider } = Layout
 
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-function App() {
-  const [count, setCount] = useState(0);
-  const [collapsed, setCollapsed] = useState(false);
+export const App = () => {
+  const [collapsed, setCollapsed] = useState(false)
   const {
     token: { colorBgContainer },
-  } = theme.useToken();
+  } = theme.useToken()
 
-  type MenuItem = Required<MenuProps>['items'][number];
+  type MenuItem = Required<MenuProps>["items"][number]
 
-  function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-  ): MenuItem {
+  function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
     return {
       key,
       icon,
       children,
       label,
-    } as MenuItem;
+    } as MenuItem
   }
 
-  const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-      getItem('Tom', '3'),
-      getItem('Bill', '4'),
-      getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
-  ];
+  const items: MenuItem[] = [getItem("Option 1", "1", <UserOutlined />), getItem("Option 2", "2", <DesktopOutlined />), getItem("User", "sub1", <UserOutlined />, [getItem("Tom", "3"), getItem("Bill", "4"), getItem("Alex", "5")]), getItem("Team", "sub2", <TeamOutlined />, [getItem("Team 1", "6"), getItem("Team 2", "8")]), getItem("Files", "9", <FileOutlined />)]
 
   return (
     <Layout className="layout">
       <Sider collapsible collapsed={collapsed} onCollapse={(value: boolean | ((prevState: boolean) => boolean)) => setCollapsed(value)}>
-        <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        <div style={{ height: 32, margin: 16, background: "rgba(255, 255, 255, 0.2)" }} />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" items={items} />
       </Sider>
       <Layout className="site-layout">
         <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '0 16px' }}>
-          <div style={{ padding: 24, background: "fff000" }}>
-            Bill is a cat.
-          </div>
-          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
-            Bill is a cat.
-          </div>
+        <Content style={{ margin: "0 16px" }}>
+          <div style={{ padding: 24, background: "fff000" }}>Bill is a cat.</div>
+          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>Bill is a cat.</div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>cristianmelo frontdev ©2023</Footer>
+        <Footer style={{ textAlign: "center" }}>cristianmelo frontdev ©2023</Footer>
       </Layout>
     </Layout>
   )
 }
-
-export default App
