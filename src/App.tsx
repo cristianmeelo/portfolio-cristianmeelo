3b373b1d401b5d1109a2820449a869cab98d96ef
cristianmeelo <cristianmeelo@gmail.com>
2023-03-20 21:36:54 -0300

    🧪 test: testing deploy

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ export const App = () => {
       <Layout className="site-layout">
         <Header style={{ padding: 0, background: colorBgContainer }} />
         <Content style={{ margin: "0 16px" }}>
-          <div style={{ padding: 24, background: "fff000" }}>Bill is a cat.</div>
+          <div style={{ padding: 24, background: "fff000" }}>Bill is a dog.</div>
           <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>Bill is a cat.</div>
         </Content>
         <Footer style={{ textAlign: "center" }}>cristianmelo frontdev ©2023</Footer>
